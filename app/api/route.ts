73418142a87b18459428d4e30d4cd1a23b2a5069
Brent Zurek <zburns1980@gmail.com>
2025-03-31 23:48:44 -0600

    removes tutorial and adds workout-logs

---
+++ app/api/route.ts
@@ -0,0 +1,9 @@
+// import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  console.log('inside GET request');
+
+  console.log('again', request.url);
+
+  return Response.json({ test: 'test4' });
+}
